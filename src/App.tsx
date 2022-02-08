b42d0a1ab94da36ee3e242b50e9802c937d964ae
pawaox4th <pawaox4th@gmail.com>
2022-02-08 23:38:22 +0700

    chore: ♻️ Setup Project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+function App() {
+  return (
+    <div
+      className="App"
+      style={{
+        width: "100%",
+        minHeight: "100vh",
+        backgroundColor: "#282c34"
+      }}></div>
+  )
+}
+
+export default App
