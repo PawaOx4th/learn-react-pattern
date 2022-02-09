551f299727842ae5e68d76a56e6108ca85900fa1
monarch02 <pawaox4th@gmail.com>
2022-02-09 13:20:38 +0700

    feat: ✨ Create render-props has success

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
-import { useState } from "react"
+import { memo, useState } from "react"
+
+type ToggleProps = { isOpen: boolean; toggle: () => void }
+
+function ToggleComponent({
+  render
+}: {
+  render: (props: ToggleProps) => JSX.Element
+}) {
+  const [isToggle, setIsToggle] = useState(true)
+  const onToggle = () => {
+    setIsToggle((isToggle) => !isToggle)
+  }
+  console.warn("App is renderer")
+  return render({
+    isOpen: isToggle,
+    toggle: onToggle
+  })
+}
+
+const WrapperToggle = memo(ToggleComponent)
 
 function App() {
+  const [counter, setCounter] = useState(0)
+
   return (
     <div
       className="App"
@@ -8,7 +30,26 @@ function App() {
         width: "100%",
         minHeight: "100vh",
         backgroundColor: "#282c34"
-      }}></div>
+      }}>
+      <button onClick={() => setCounter((latestValue) => latestValue + 1)}>
+        {counter}
+      </button>
+      <WrapperToggle
+        render={({ isOpen, toggle }) => {
+          return (
+            <>
+              <h2
+                style={{
+                  color: "red"
+                }}>
+                {JSON.stringify(isOpen)}
+              </h2>
+              <button onClick={() => toggle()}>ON TOGGLE</button>
+            </>
+          )
+        }}
+      />
+    </div>
   )
 }
 
